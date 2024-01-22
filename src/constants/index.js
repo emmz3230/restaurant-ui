fcb0082196d7be24022937b03c70af837144ee80
emmz3230 <emmz3230@gmail.com>
2024-01-22 05:22:50 +0100

    image

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,9 @@
-import images from './images';
-import meal from '../assets/meal.mp4';
-import data from './data';
+import images from "./images";
+import meal from "../asset s/meal.mp4";
+import data from "./data";
 
 export { images, meal, data };
+
+0720675014
+access bank
+afeez adebayo
